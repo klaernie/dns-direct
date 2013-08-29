klaernie/dns-direct

commit 36228dce6cacf7af9a77b476f26fb6b5faba33f6

replace old debug handling by a proper function

dns-direct.sh
@@ -16,13 +16,25 @@ source dns-direct.conf
 DEBUG=1
 DEBUG=`test "$*" = "debug" ; echo $?`
 
+# define a function to output debugging info
+function debug(){
+	if $DEBUG -eq 0
+	then
+		echo $@
+	fi
+}
+
 FORCE=0
 
 OldIP=$(dig @$NS +short +tries=10 $HOST 2>>tmpfile)
-[ $DEBUG -eq 0 ] && echo OldIP: $OldIP
+
+debug "OldIP: $OldIP"
+
 [ $FORCE -eq 1 ] && OldIP="x.x.x."
 CurreIP=$(curl -sS -3 -4 --user-agent "curl/7.21.0 (i486-pc-linux-gnu) dnsactual" $CHECK_URL 2>>tmpfile)
-[ $DEBUG -eq 0 ] && echo NewIP: $CurreIP
+
+debug "NewIP: $CurreIP"
+
 if [ -z "$CurreIP" -o -z "$OldIP" ]
 then
 	echo "Retrieval of old or new IP failed:"
@@ -33,7 +45,7 @@ fi
 if [ "$CurreIP" = "$OldIP" ]
 then
 	# Both IPs are equal
-	[ $DEBUG -eq 0 ] && echo "Update not required..."
+	debug "Update not required..."
 	exit 0
 else
 	# The IP might have changed

commit 1edc967a2ccdf97cfbca7b37df2b9ef30857237d

fix error introduced in 36228dce: need to call [] in if

dns-direct.sh
@@ -21,7 +21,7 @@ DEBUG=`test "$*" = "debug" ; echo $?`
 
 # define a function to output debugging info
 function debug(){
-	if $DEBUG -eq 0
+	if [ $DEBUG -eq 0 ]
 	then
 		echo $@
 	fi